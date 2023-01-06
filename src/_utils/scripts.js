10f0991d81f00e55ca2b3bfe84f3b560782f36b0
Clint Mossman <cmossman@pima.edu>
2023-01-06 07:46:49 -0700

    Added js for footer

--- src/_utils/scripts.js
+++ src/_utils/scripts.js
@@ -17,3 +17,37 @@ getAccords.forEach((accord) => {
     body.toggleAttribute("hidden");
   });
 });
+
+// Footnotes toggle
+const toggleBtns = document.querySelectorAll(".toggle-btn, .toggle-footnotes");
+
+const changeFootnotesText = (toggleBtns,toggleBtn) => {
+  if(toggleBtns[toggleBtn].innerHTML == "[Show Footnotes]") {
+    toggleBtns[toggleBtn].innerHTML = "[Hide Footnotes]";
+  } else {
+    toggleBtns[toggleBtn].innerHTML = "[Show Footnotes]";
+  }
+}
+
+if (document.querySelector(".toggle-btn") || document.querySelector(".toggle-footnotes")) {
+
+
+  for (let toggleBtn = 0; toggleBtn < toggleBtns.length; toggleBtn++) {
+    // Add tabindex
+    toggleBtns[toggleBtn].setAttribute("tabindex", "0");
+
+    // Show/hide on click
+    toggleBtns[toggleBtn].addEventListener("click", () => {      
+      toggleBtns[toggleBtn].nextElementSibling.classList.toggle("show");      
+      changeFootnotesText(toggleBtns,toggleBtn)
+    })
+
+    // Show/hide on enter for users who use tab
+    toggleBtns[toggleBtn].addEventListener("keydown", (enter) => {
+      if (enter.keyCode === 13) {
+        toggleBtns[toggleBtn].nextElementSibling.classList.toggle("show");
+        changeFootnotesText(toggleBtns,toggleBtn)
+      }
+    })
+  }
+}
